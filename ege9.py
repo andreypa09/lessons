d2d6fb1fe8bc3733e1344a48c0a3796feb9d9c44
andreypa09 <andrey.pavlovsky09@gmail.com>
2025-12-25 18:24:02 +0200

    Add .gitignore

--- ege9.py
+++
@@ -1,8 +0,0 @@
-# with open('9.txt', 'r') as f:
-#     k = 0
-#     for line in f:
-#         a = sorted([int(x) for x in line.split()])
-#         if :
-#             if a[1] * a[2] <= 1000:
-#                 k += 1
-#     print(k)
