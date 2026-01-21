b5495676730e45d50e63fd511135148901f6e550
andreypa09 <andrey.pavlovsky09@gmail.com>
2026-01-21 19:32:30 +0200

    add lesson

---
+++ lessons/21.01.2026.py
@@ -0,0 +1,75 @@
+# map(функция, список(итерируемый объект))
+# map - Это метод, который позволяет применить функцию к каждому элементу последовательности
+
+
+# def plus5(x):
+#     return x + 5
+# a = [1,1,1,1,1]
+# print(list(map(lambda x: x + 5, a)))
+
+# user = [
+#     {"username": "daniel", "age": 10},
+#     {"username": "daniel1", "age": 10},
+#     {"username": "daniel2", "age": 10},
+#     {"username": "daniel3", "age": 10}
+# ]
+# print(list(map(lambda x: x["username"], user)))
+
+# filter()
+# nums = list(range(10))
+# [0,1,2,3,4,5,6,7,8,9]
+# [True,False ... False]
+# evens = list(filter(lambda x: x % 2 == 0, nums))
+# print(evens)
+
+# def plus():
+
+# def minus():
+
+# def ss(fplus, fminus):
+#     return fplus, fminus
+
+
+
+
+# nums = [1, 2 , 3, 4, 5]
+# square_nums = list(map(lambda x: x * x, nums))
+# print(square_nums)
+#
+# nums = list(range(1,11))
+# not_evens = list(filter(lambda x: x % 2 == 1, nums))
+# print(not_evens)
+
+# words = ['apple', 'banana', 'cherry', 'date']
+# len_words = list(map(len, words))
+# print(len_words)
+
+# nums = [-5, -2, 0, 3, 7, -1, 10]
+# positive_nums = list(filter(lambda num: num >= 0, nums))
+# print(positive_nums)
+
+# a = [1,1,1,1,1,1]
+# b = [2,2,2,2,2,2]
+# print(list(map(lambda x,y: x + y, a, b)))
+
+# list1 = [1,2,3,4]
+# list2 = [5,6,7,8]
+# print(list(map(lambda x,y: x * y, list1, list2)))
+
+# def is_simple(x):
+#     if 0 <= x <= 2:
+#         return False
+#     for i in range(2, int(x / 2) + 1):
+#         if x % i == 0:
+#             return False
+#     return True
+# nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# print(list(filter(is_simple, nums)))
+
+def my_map(lambda1, nums1):
+    result = []
+    for num in nums1:
+        result.append(lambda1(num))
+    return result
+nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+print(my_map(lambda x: x, nums))
